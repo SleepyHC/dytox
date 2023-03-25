f7d88e5ca51653eb21e3426730ccef84fe78cacb
SleepyHC <72695009+SleepyHC@users.noreply.github.com>
2023-03-25 22:51:20 +0800

    debug

--- continual/datasets.py
+++ continual/datasets.py
@@ -135,8 +135,8 @@ def build_transform(is_train, args):
                 transform.transforms[0] = transforms.RandomCrop(
                     args.input_size, padding=4)
 
-            if args.input_size == 32 and args.data_set == 'CIFAR':
-                transform.transforms[-1] = transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761))
+            # if args.input_size == 32 and args.data_set == 'CIFAR':
+            #     transform.transforms[-1] = transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761))
             return transform
 
         t = []
@@ -148,10 +148,10 @@ def build_transform(is_train, args):
             t.append(transforms.CenterCrop(args.input_size))
 
         t.append(transforms.ToTensor())
-        if args.input_size == 32 and args.data_set == 'CIFAR':
-            # Normalization values for CIFAR100
-            t.append(transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)))
-        else:
-            t.append(transforms.Normalize(IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD))
+        # if args.input_size == 32 and args.data_set == 'CIFAR':
+        #     # Normalization values for CIFAR100
+        #     t.append(transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)))
+        # else:
+        #     t.append(transforms.Normalize(IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD))
 
         return transforms.Compose(t)
