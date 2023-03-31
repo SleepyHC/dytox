c296bf688619a174e220f9f08888cf8dd59b35c7
SleepyHC <72695009+SleepyHC@users.noreply.github.com>
2023-03-31 10:48:11 +0800

    pgd_grad

--- continual/engine.py
+++ continual/engine.py
@@ -40,7 +40,7 @@ def pgd_attack(model, images,device, labels, eps=8/255, alpha=10/255, iters=1) :
         else:
             main_output = outputs
         model.zero_grad()
-        cost = loss(main_output, labels).to(device)
+        cost = loss(main_output, labels).to(device).requires_grad_()
         cost.backward()
         # 图像 + 梯度得到对抗样本
         adv_images = images + alpha*images.grad.sign()
